c89af5133914b04aa4ab017383e5d567e7bcb8db
duongkhaivinh2008-cyber <duongkhaivinh.2008@gmail.com>
2025-11-02 12:07:46 +0700

    add file java and update file html

---
+++ index.js
@@ -0,0 +1,18 @@
+
+console.log("Hello World");
+
+let name = "firstName";
+let lastName = "lastName";
+let email = "duongKhaiVinh.2008@Gamil.Com";
+let dateCreate = "11/4/2000";
+let dateUpdate = "12/5/2004";
+let dateDelete = "15/4/2025";
+
+
+let str1 = "hello";
+let str2 = "world";
+let result = str1 + str2;
+console.log("result"); //  hellơ world
+
+let str = 'Hello World!!!';
+console.log(('Số ký tự trong chuỗi là:', str.length)); //14
